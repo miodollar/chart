5c97c763dedf94256abc6963743258d15e42f5ad
miodollar <36316189+miodollar@users.noreply.github.com>
2018-02-12 07:24:52 -0500

    Update addjson.php

--- addjson.php
+++ addjson.php
@@ -1,6 +1,9 @@
 <?php
-$myObj->date = "Monday";
+$date=date("Y/m/d");
+echo '<br><br>'.$date.'';
+$myObj->date = $date;
 $myObj->city = "New York";
+$myObj->value = round($miodollar,2);
 $myJSON = json_encode($myObj);
 echo '<br><br>'.$myJSON;
 ?>
